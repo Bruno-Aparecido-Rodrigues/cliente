905a138f03e966adf73aa741f50c7f230fae971f
Bruno-Aparecido-Rodrigues <aparecidobruno749@gmail.com>
2026-04-26 13:48:25 -0300

    correção

--- spring/src/main/java/io/github/fatec/controller/ClienteController.java
+++ spring/src/main/java/io/github/fatec/controller/ClienteController.java
@@ -5,7 +5,7 @@ import io.github.fatec.controller.dto.request.ClienteRequest;
 import io.github.fatec.controller.dto.request.ClienteUpdateRequest;
 import io.github.fatec.controller.dto.response.ClienteResponse;
 import io.github.fatec.entity.Cliente;
-import io.github.fatec.service.ClienteService;
+import io.github.fatec.repository.ClienteRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,17 +15,17 @@ import java.util.List;
 @RequestMapping("/fatec/clientes")
 public class ClienteController {
 
-    private final ClienteService service;
+    private final ClienteRepository repository;
 
-    public ClienteController(ClienteService service) {
-        this.service = service;
+    public ClienteController(ClienteRepository repository) {
+        this.repository = repository;
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ClienteResponse criar(@RequestBody ClienteRequest request) {
         Cliente cliente = ClienteControllerAdapter.cast(request);
-        Cliente criado = service.criar(cliente);
+        Cliente criado = repository.salvar(cliente);
         return ClienteControllerAdapter.toResponse(criado);
     }
 
@@ -33,20 +33,20 @@ public class ClienteController {
     @PutMapping
     public ClienteResponse atualizar(@RequestBody ClienteUpdateRequest request) {
         Cliente cliente = ClienteControllerAdapter.cast(request);
-        Cliente atualizado = service.atualizar(cliente);
+        Cliente atualizado = repository.atualizar(cliente);
         return ClienteControllerAdapter.toResponse(atualizado);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deletar(@PathVariable String id) {
-        service.deletar(id);
+        repository.deletar(id);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public List<ClienteResponse> listar() {
-        return service.listar()
+        return repository.listar()
                 .stream()
                 .map(ClienteControllerAdapter::toResponse)
                 .toList();
@@ -55,7 +55,8 @@ public class ClienteController {
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
     public ClienteResponse buscarPorId(@PathVariable String id) {
-        Cliente cliente = service.buscarPorId(id);
+        Cliente cliente = repository.buscarPorId(id)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado: " + id));
         return ClienteControllerAdapter.toResponse(cliente);
     }
 }
